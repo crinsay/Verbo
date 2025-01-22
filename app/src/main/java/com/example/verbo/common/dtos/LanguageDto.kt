6d5620fa0016d3e484cfd68858de380b178b2f54
Łukasz Karwacki <lkarwacki3@interia.pl>
2025-01-22 23:53:40 +0100

    Feature: Language entity logic done. Dao, Repository + Dto mapping.

---
+++ app/src/main/java/com/example/verbo/common/dtos/LanguageDto.kt
@@ -0,0 +1,6 @@
+package com.example.verbo.common.dtos
+
+data class LanguageDto(
+    val languageId: Long,
+    val name: String
+)
--- app/src/main/java/com/example/verbo/models/repositories/languagerepository/LanguageRepository.kt
+++ app/src/main/java/com/example/verbo/models/repositories/languagerepository/LanguageRepository.kt
@@ -1,4 +1,41 @@
 package com.example.verbo.models.repositories.languagerepository
 
-class LanguageRepository : ILanguageRepository {
+import com.example.verbo.common.dtos.LanguageDto
+import com.example.verbo.common.mappers.mapToLanguage
+import com.example.verbo.common.mappers.mapToLanguageDto
+import com.example.verbo.common.mappers.mapToLanguageDtos
+import com.example.verbo.models.daos.LanguageDao
+import javax.inject.Inject
+
+class LanguageRepository @Inject constructor(
+    private val languageDao: LanguageDao
+) : ILanguageRepository {
+    override suspend fun insertLanguage(newLanguageDto: LanguageDto) {
+        val newLanguage = newLanguageDto.mapToLanguage()
+        languageDao.insertLanguage(newLanguage)
+    }
+
+    override suspend fun updateLanguage(updatedLanguageDto: LanguageDto) {
+        val updatedLanguage = updatedLanguageDto.mapToLanguage()
+        languageDao.updateLanguage(updatedLanguage)
+    }
+
+    override suspend fun deleteLanguage(languageToDeleteDto: LanguageDto) {
+        val languageToDelete = languageToDeleteDto.mapToLanguage()
+        languageDao.deleteLanguage(languageToDelete)
+    }
+
+    override suspend fun getAllLanguages(): List<LanguageDto> {
+        val languages = languageDao.getAllLanguages()
+        val languageDtos = languages.mapToLanguageDtos()
+
+        return languageDtos
+    }
+
+    override suspend fun getLanguageById(languageId: Long): LanguageDto {
+        val language = languageDao.getLanguageById(languageId)
+        val languageDto = language.mapToLanguageDto()
+
+        return languageDto
+    }
 }
